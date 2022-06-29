2a5201e8b5357319009b7a9ed8f6313cccfd2fb0
Andrey <ayk-82@mail.ru>
2022-06-29 13:31:26 +0300

    fix dev url

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 4444;
 const app = express();
 app.use(cors());
 app.use('/images', express.static('images'));
+app.get('/images/*', (req, res) => {
+  res.sendFile(__dirname + req.url)
+})
 app.get('/data', (req, res) => {
   res.send(data.products);
 });
@@ -16,9 +19,6 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'))
-  })
-  app.get('/images/*', (req, res) => {
-    res.sendFile(__dirname + req.url)
   })
 }
 
